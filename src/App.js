0d4b6088538cc891de885b23b8fe66aa2598b780
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2022-06-26 22:20:23 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
-import Jumbotron from './components/jumbotron';
+import { FooterContainer } from './container/footer';
 import { JumbotronContainer } from './container/jumbotron';
-import jumboData from './fixtures/jumbo.json';
 
 function App() {
   return (
+    <>
    <JumbotronContainer/>
+   <FooterContainer/>
+   </>
   );
 }
 
--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -14,7 +14,7 @@ width:100%;
 export const Item=styled.div`
 display:flex;
 border-bottom:8px solid #222;
-padding:50px 5%;
+padding:10px 5%;
 color:white;
 overflow:hidden;
 `;
