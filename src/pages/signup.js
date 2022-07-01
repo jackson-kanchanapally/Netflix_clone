5967a6b15af0ab6138ba97d45b39c2ac3ac9b0e6
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2022-07-01 11:23:25 +0530

    small text added in signup

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -67,6 +67,9 @@ export default function SignUp() {
                 Already have an account ?{" "}
                 <Form.Link to={ROUTES.SIGN_IN}>Sign In</Form.Link>
               </Form.Text>
+              <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
+          </Form.TextSmall>
             </Form.Base>
           </form>
         </Form>
